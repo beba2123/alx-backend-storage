779ce20ee1d44cd9ebe3a24fffab6911be96cf83
beba2123 <antenehbeba@gmail.com>
2023-04-07 18:46:51 +0300

    forth Mysqlcommit

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+-- SQL script that ranks country origins of bands, ordered by the number of fans
 
 SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands

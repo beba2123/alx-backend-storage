8288a3fda856840e864122a214ecdc0bb86bce3f
beba2123 <antenehbeba@gmail.com>
2023-04-11 20:49:45 +0300

    .py commit in NoSQL

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Write a Python function that lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """mongo_collection will be the pymongo collection object
+       Return an empty list if no document in the collection
+    """
+    Files = mongo_collection.find()
+    if Files.count() == 0:
+        return []
+    return Files
